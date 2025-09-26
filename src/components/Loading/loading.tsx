3e1e76d3821862d9982c03573ff904c240dee571
Hannah Ingham <hannah.c.ingham@gmail.com>
2025-09-26 13:16:47 -0700

    loading component

--- src/components/Loading/loading.tsx
+++ src/components/Loading/loading.tsx
@@ -1,7 +1,7 @@
 
 const Loading = ({message}: {message: string}) => {
     return (
-        <div className="min-h-screen flex items-center justify-center w-full">
+        <div className="min-h-screen flex items-center justify-center">
             <div className="text-xl">{message}</div>
         </div>
     )
