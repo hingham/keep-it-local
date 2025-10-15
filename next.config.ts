cc4ba4d28b4a0da1eed30bba82ebd7b22657cec5
Hannah Ingham <hannah.c.ingham@gmail.com>
2025-10-15 12:31:36 -0700

    updating-config-with-image-url

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'images.unsplash.com',
+        hostname: 'thelocalboard.city',
         port: '',
         pathname: '/**',
       },
